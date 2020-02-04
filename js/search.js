0592bdef2e67c77b070483fb2d0d40829078a975
cloud-build <cloud-build@example.com>
2020-02-04 09:46:17 +0000

    Cloud Build. build id = 06a4b0e4-c36c-47f6-abbf-e3ea96118cf6, commit sha = 4dfbce94ee90baa532eacd1e183f5560b863df61

--- js/search.js
+++ js/search.js
@@ -1,29 +1,33 @@
 var lunrIndex, $results, pagesIndex;
 
 function initLunr() {
-  $.getJSON("../index.json").done(function(index) {
-    pagesIndex = index;
-    lunrIndex = lunr(function() {
-      var lunrConfig = this;
-      lunrConfig.use(lunr.multiLanguage('en', 'jp'));
-      lunrConfig.ref("href");
-      lunrConfig.field("title", { boost: 10 });
-      lunrConfig.field("contents");
-      pagesIndex.forEach(function(page) {
-        lunrConfig.add(page);
+  $.getJSON("../index.json")
+    .done(function(index) {
+      pagesIndex = index;
+      lunrIndex = lunr(function() {
+        var lunrConfig = this;
+        lunrConfig.use(lunr.multiLanguage("en", "jp"));
+        lunrConfig.ref("href");
+        lunrConfig.field("categories", { boost: 1 });
+        lunrConfig.field("date", { boost: 1 });
+        lunrConfig.field("key", { boost: 5 });
+        lunrConfig.field("tags", { boost: 1 });
+        lunrConfig.field("title", { boost: 10 });
+        pagesIndex.forEach(function(page) {
+          lunrConfig.add(page);
+        });
       });
+    })
+    .fail(function(jqxhr, textStatus, error) {
+      var err = textStatus + ", " + error;
+      console.error("Error getting Hugo index flie:", err);
     });
-  })
-  .fail(function(jqxhr, textStatus, error) {
-    var err = textStatus + ", " + error;
-    console.error("Error getting Hugo index flie:", err);
-  });
 }
 
-function search(){
+function search() {
   $results = $("#results");
   $results.empty();
-  var query = document.getElementById('search-query').value;
+  var query = document.getElementById("search-query").value;
   if (query.length < 2) {
     return;
   }
@@ -31,20 +35,20 @@ function search(){
 }
 
 function results(query) {
-  return lunrIndex.search(`*${query}*`).map(function(result) {
+  return lunrIndex.search(`${query}`).map(function(result) {
     return pagesIndex.filter(function(page) {
-        return page.href === result.ref;
+      return page.href === result.ref;
     })[0];
   });
 }
 
 function renderResults(results) {
   if (!results.length) {
-    $results.append('<p>No matches found</p>');
+    $results.append("<p>No matches found</p>");
     return;
   }
 
-  results = results.sort((x, y) => x.date > y.date)
+  results = results.sort((x, y) => x.date < y.date);
   results.forEach(function(result) {
     var $result = $("<li>", { class: "post_search_item" });
 
@@ -53,17 +57,10 @@ function renderResults(results) {
     var $title = $("<a>", { href: result.href });
     $title.append($("<h3>", { class: "post_link", text: result.title }));
     $excerpt.append($title);
-
     $excerpt.append($("<p>", { text: result.date }));
-
-    length = 100;
-    if (length > result.contents.length) { length = result.contents.length; }
-    $excerpt.append($("<p>", { text: result.contents.slice(0, length) + " ..." }));
-
     $result.append($excerpt);
     $results.append($result);
   });
 }
 
 initLunr();
-
